86a6c2989e52d5b2e6e26cea9030b277a0899264
Pedro H <pedro_gonsalves@hotmail.com>
2023-05-04 00:59:06 -0300

    add: text parser

--- src/main/scala/translator/text_test.scala
+++ src/main/scala/translator/text_test.scala
@@ -1,19 +1,34 @@
 package translator
 
-import translator.Files_management.createPdfFile
 
-object text_test {
+import translator.Translator.translator
 
-    def textFormat(text:String): Unit = {
-        //val text = "Constructive Type theory has been a topic of research interest to computer\nscientists, mathematicians, logicians and philosophers for a number of years.\nFor computer scientists it provides a framework which brings together logic\nand programming languages in a most elegant and fertile way: program\ndevelopment and verification can proceed within a single system. Viewed\nin a different way, type theory is a functional programming language with\nsome novel features, such as the totality of all its functions, its expressive\ntype system allowing functions whose result type depends upon the value\nof its input, and sophisticated modules and abstract types whose interfaces\ncan contain logical assertions as well as signature information. A third\npoint of view emphasizes that programs (or functions) can be extracted\nfrom proofs in the logic. "
-        val texts = text.split("\n").
-          map(textLb =>  if (textLb.length > 5000) {
-          println("This paragraph is longer.")
-        } else {
-            println("This paragraph is acceptably.")
-          })
+object text_test extends App {
 
+    def textFormat(text:String, split:String = "\n"): String = {
+          val treatmentText = if (text.length > 5000) {
+            text.split(split).map(t => translator(text = t)).mkString
+          }
+          else {
+            translator(text = text)
+          }
+      treatmentText
     }
-    //val x = textFormat()
-    //println(x)
+    val x = textFormat(text="""In conversations at technical conferences, I often hear the same question:
+  How can I learn more about database internals? I don’t even know where
+  to start.” Most of the books on database systems do not go into details of
+  storage engine implementation, and cover the access methods, such as BTrees,
+  on a rather high level. There are very few books that cover more
+  recent concepts, such as different B-Tree variants and log-structured
+  storage, so I usually recommend reading papers.
+  Everyone who reads papers knows that it’s not that easy: you often lack
+  context, the wording might be ambiguous, there’s little or no connection
+  between papers, and they’re hard to find. This book contains concise
+  summaries of important database systems concepts and can serve as a
+  guide for those who’d like to dig in deeper, or as a cheat sheet for those
+  already familiar with these concepts.
+  Not everyone wants to become a database developer, but this book will
+  help people who build software that uses database systems: software
+  developers, reliability engineers, architects, and engineering managers.""")
+    println(x)
 }
