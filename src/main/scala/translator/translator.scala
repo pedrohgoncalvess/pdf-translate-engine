39c2f31e67596b363d8e15e1446237d91cc08e7c
Pedro H <pedro_gonsalves@hotmail.com>
2023-05-25 14:24:35 -0300

    add: implementing async reqs and perform some refactors

--- src/main/scala/translator/translator.scala
+++ src/main/scala/translator/translator.scala
@@ -4,8 +4,71 @@ case class ScalaTranslator(target_language:String = "pt", source_language:String
 
   import java.net.URLEncoder
   import org.jsoup.Jsoup
+  import scala.concurrent.Future
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.concurrent.{Await, Future}
+  import scala.util.{Failure, Success}
+  import requests.Response
+  import scala.concurrent.duration.DurationInt
 
-  def translator(text: String): String = {
+  def translator(text: String, separator: String = "\n"): String = {
+    val translatedText = if (text.length > 5000) {
+      val rawTranslatedText: String = textParser(text, separator)
+      rawTranslatedText
+    } else {
+      val rawTranslatedText: String = rawTranslator(text)
+      rawTranslatedText
+    }
+
+    translatedText
+  }
+
+  def asyncTranslator(text: String): Future[String] = {
+
+    if (text.length < 5000) {
+
+      val escaped_text = URLEncoder.encode(text, "UTF-8").replace("+", "%20")
+      val url = s"https://translate.google.com/m?tl=${this.target_language}&sl=${this.source_language}&q=$escaped_text"
+
+      val r: Future[Response] = Future {
+        requests.get(url)
+      }
+      r.map { response =>
+        val doc = Jsoup.parse(response.text())
+        val result = doc.select(".result-container").text()
+        result
+      }
+    }
+    else {
+      println("This text is longer than 5000 characters.")
+      val toReturn: Future[String] = Future {"None"}
+      toReturn
+    }
+
+}
+
+    private def textParser(text: String, split:String): String = {
+      val treatmentText:String = {
+            val splitText = text.split(split)
+
+            var arrayString: String = ""
+            val textNoLonger: String = splitText.map(textA => {
+              if (textA.length < 5000 - arrayString.length) {
+                arrayString = arrayString + textA
+              } else {
+                val textReturn = this.rawTranslator(text = arrayString)
+                arrayString = ""
+                textReturn
+              }
+            }
+            ).mkString.replace("(", "").replace(")", "")
+
+            textNoLonger
+          }
+          treatmentText
+    }
+
+   private def rawTranslator(text: String): String = {
 
     val escaped_text = URLEncoder.encode(text, "UTF-8").replace("+", "%20")
 
@@ -23,7 +86,7 @@ case class ScalaTranslator(target_language:String = "pt", source_language:String
     val textPagesEn = readPdfFiles(pdf.path, startIn = pdf.startIn, endIn = pdf.endIn)
 
     val textPagesPt = textPagesEn.map(text => {
-      val textPage = this.translator(text)
+      val textPage = this.rawTranslator(text)
       textPage
     })
 
